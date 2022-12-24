da9cdbf94cdc5750dbb3b8c5ec28378fb52d3a32
Oleh Shynkarchuk <oleh.shynkarchuk@gmail.com>
2022-12-24 18:27:50 +0200

    feat(EEJT-0002) redesigned controller logic, all error situation moved to services

--- src/main/java/com/epam/esm/giftcertificates/controller/GiftsController.java
+++ src/main/java/com/epam/esm/giftcertificates/controller/GiftsController.java
@@ -1,5 +1,6 @@
 package com.epam.esm.giftcertificates.controller;
 
+
 import com.epam.esm.giftcertificates.entity.GiftCertificate;
 import com.epam.esm.giftcertificates.service.GiftCertificatesService;
 
@@ -8,19 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 @RestController
 @RequestMapping(value = "/gifts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GiftsController {
+
+    private final GiftCertificatesService giftCertificatesService;
     @Autowired
-    GiftCertificatesService giftCertificatesService;
+    public GiftsController(GiftCertificatesService giftCertificatesService) {
+        this.giftCertificatesService = giftCertificatesService;
+    }
 
     @GetMapping()
     public ResponseEntity<List<GiftCertificate>> getAllTags() {
@@ -28,96 +29,32 @@ public class GiftsController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getOneGiftCertificate(@PathVariable("id") Long id) {
-        if (id > 0) {
-            Optional<GiftCertificate> read = giftCertificatesService.read(id);
-            if (read.isPresent()) {
-                return new ResponseEntity<>(read.get(), HttpStatus.OK);
-//                return new ResponseEntity<>(new GiftCertificate(1L,"Gift","",new BigDecimal("245.12"), Duration.ZERO,List.of(new Tag(1L,"Discount"),new Tag(2L,"free"))), HttpStatus.OK);
-            }
-            return new ResponseEntity<>(List.of(Map.of("errorMessage", "Requested tag not found (id = " + id + ")"),
-                    Map.of("errorCode", HttpStatus.NOT_FOUND)), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (id = " + id + ") id can be from 1 up to ∞"),
-                Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<GiftCertificate> getOneGiftCertificate(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(giftCertificatesService.read(id), HttpStatus.OK);
     }
 
-    @GetMapping("/get")
-    public ResponseEntity<?> getOneGiftCertificate(
+    @GetMapping(value = "/")
+    public ResponseEntity<List<GiftCertificate>> getOneGiftCertificate(
             @RequestParam(required = false) String tagName,
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String description,
             @RequestParam(required = false) String sortByDate,
             @RequestParam(required = false) String sortByName) {
-        boolean validation = dataValidation(tagName, name, description, sortByName, sortByName);
-        if (validation) {
-            String sqlQuery = "SELECT * FROM giftcertificate gc" +
-                    " INNER JOIN giftcertificate_has_tag ght" +
-                    " ON ght.giftcertificate_id = gc.id" +
-                    " INNER JOIN tags t" +
-                    " ON t.id = ght.tag_id" +
-                    " WHERE" + (tagName != null ?
-                    " t.name = '" + tagName + "' AND" : "");
-            sqlQuery = sqlQuery + (name != null ? " gc.name LIKE '%" + name + "%'" + (description != null ? " AND gc.description LIKE '%" + description + "%'" : "") : (description != null ? " gc.description LIKE '%" + description + "%'" : ""));
-            sqlQuery = sqlQuery + (sortByDate != null ? " ORDER BY gc.create_date " + sortByDate + "," + (sortByName != null ? "gc.name " + sortByName : "") : (sortByName != null ? " ORDER BY gc.name " + sortByName : ""));
-            System.out.println("sqlQuery = " + sqlQuery);
-            Optional<GiftCertificate> read = giftCertificatesService.read(sqlQuery);
-            return null;
-        } else return null;
+        return new ResponseEntity<>(giftCertificatesService.read(tagName,name,description,sortByDate,sortByName), HttpStatus.OK);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createNewTag(@RequestBody GiftCertificate giftCertificate) {
-        System.out.println("gft = " +giftCertificate);
-        Optional<GiftCertificate> read = giftCertificatesService.create(giftCertificate);
-         if (read.isPresent()) {
-                return new ResponseEntity<>(read.get(), HttpStatus.CREATED);
-            }
-//        }
-//          {
-//                return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (" + read.get() + ") already exist or something went wrong"),
-//                        Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
-//            }
-//        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "An error occurrence while creating new tag"),
-                Map.of("errorCode", HttpStatus.INTERNAL_SERVER_ERROR)), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<GiftCertificate> createNewTag(@RequestBody GiftCertificate giftCertificate) {
+        return new ResponseEntity<>(giftCertificatesService.create(giftCertificate), HttpStatus.CREATED);
     }
 
     @PatchMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> patchNewTag(@PathVariable("id") Long id, @RequestBody GiftCertificate giftCertificate) {
-        Optional<GiftCertificate> read = giftCertificatesService.read(id);
-        if (read.isPresent()) {
-            boolean isCreated = giftCertificatesService.update(id, giftCertificate);
-            if (isCreated) {
-                    return new ResponseEntity<>(read.get(), HttpStatus.CREATED);
-            }
-            return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (" + read.get() + ") already exist or something went wrong"),
-                    Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "An error occurrence while creating new tag"),
-                Map.of("errorCode", HttpStatus.INTERNAL_SERVER_ERROR)), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<GiftCertificate> patchNewTag(@PathVariable("id") Long id, @RequestBody GiftCertificate giftCertificate) {
+        return new ResponseEntity<>(giftCertificatesService.update(id, giftCertificate), HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteTagsById(@PathVariable("id") Long id) {
-        if (id > 0L) {
-            Optional<GiftCertificate> tagIsExist = giftCertificatesService.read(id);
-            if (tagIsExist.isPresent()) {
-                boolean tagIsDeleted = giftCertificatesService.delete(id);
-                if (tagIsDeleted) {
-                    return new ResponseEntity<>(tagIsExist.get(), HttpStatus.OK);
-                } else
-                    return new ResponseEntity<>(List.of(Map.of("errorMessage", "An error occurred while deleting the certificate"),
-                            Map.of("errorCode", HttpStatus.INTERNAL_SERVER_ERROR)), HttpStatus.INTERNAL_SERVER_ERROR);
-            } else
-                return new ResponseEntity<>(List.of(Map.of("errorMessage", "Requested the certificate was not found (id = " + id + ")"),
-                        Map.of("errorCode", HttpStatus.NOT_FOUND)), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (id = " + id + ") id can`t be negative or 0"),
-                Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
-    }
-
-    private boolean dataValidation(String tagName, String name, String description, String sortByName, String sortByName1) {
-        return true;
+        return new ResponseEntity<>(giftCertificatesService.delete(id), HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/epam/esm/tags/controller/TagsController.java
+++ src/main/java/com/epam/esm/tags/controller/TagsController.java
@@ -10,14 +10,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping(value = "/tags", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TagsController {
+    private final TagsService tagsService;
+
     @Autowired
-    TagsService tagsService;
+    public TagsController(TagsService tagsService) {
+        this.tagsService = tagsService;
+    }
 
     @GetMapping()
     public ResponseEntity<List<Tag>> getAllTags() {
@@ -26,53 +29,16 @@ public class TagsController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getOneTag(@PathVariable("id") Long id) {
-        if (id > 0) {
-            Optional<Tag> read = tagsService.read(id);
-            if (read.isPresent()) {
-                return new ResponseEntity<>(read.get(), HttpStatus.OK);
-            }
-            return new ResponseEntity<>(List.of(Map.of("errorMessage", "Requested tag not found (id = " + id + ")"),
-                    Map.of("errorCode", HttpStatus.NOT_FOUND)), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (id = " + id + ") id can be from 1 up to ∞"),
-                Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(tagsService.read(id), HttpStatus.OK);
     }
 
-    @PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createNewTag(@RequestBody Tag tag) {
-        boolean isCreated = tagsService.create(tag);
-        if (isCreated) {
-            Optional<Tag> read = tagsService.read(tag.getName());
-            if (read.isPresent()) {
-                return new ResponseEntity<>(read.get(), HttpStatus.CREATED);
-            }
-        } else {
-            Optional<Tag> read = tagsService.read(tag.getName());
-            if (read.isPresent()) {
-                return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (" + read.get() + ") already exist or something went wrong"),
-                        Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
-            }
-        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "An error occurrence while creating new tag"),
-                Map.of("errorCode", HttpStatus.INTERNAL_SERVER_ERROR)), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(tagsService.create(tag), HttpStatus.CREATED);
     }
 
-    @DeleteMapping(value = "/{id}/delete")
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteTagsById(@PathVariable("id") Long id) {
-        if (id > 0L) {
-            Optional<Tag> tagIsExist = tagsService.read(id);
-            if (tagIsExist.isPresent()) {
-                boolean tagIsDeleted = tagsService.delete(id);
-                if (tagIsDeleted) {
-                    return new ResponseEntity<>(tagIsExist.get(), HttpStatus.OK);
-                } else
-                    return new ResponseEntity<>(List.of(Map.of("errorMessage", "Something went wrong while deleting tag"),
-                            Map.of("errorCode", HttpStatus.INTERNAL_SERVER_ERROR)), HttpStatus.INTERNAL_SERVER_ERROR);
-            } else
-                return new ResponseEntity<>(List.of(Map.of("errorMessage", "Requested tag not found (id = " + id + ")"),
-                        Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(List.of(Map.of("errorMessage", "Bad request (id = " + id + ") id can be from 1 up to ∞"),
-                Map.of("errorCode", HttpStatus.BAD_REQUEST)), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(tagsService.delete(id), HttpStatus.NO_CONTENT);
     }
 }
