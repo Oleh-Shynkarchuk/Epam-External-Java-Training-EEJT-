eca50841fdc7d8c891dd28b69108845f0ac23324
Oleh Shynkarchuk <oleh.shynkarchuk@gmail.com>
2023-01-03 17:22:00 +0200

    feat(EEJT-0002) prototype exception class for future functionality

---
+++ src/main/java/com/epam/esm/integration/errorhandle/ItemNotFound.java
@@ -0,0 +1,21 @@
+package com.epam.esm.integration.errorhandle;
+
+import org.springframework.http.HttpStatus;
+
+public class ItemNotFound extends RuntimeException{
+    private final int errorCode;
+    private final HttpStatus httpStatus;
+    public ItemNotFound(String message, int errorCode, HttpStatus status) {
+        super(message);
+        this.errorCode=errorCode;
+        this.httpStatus=status;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
