88c394cff0e67f8ca04a051764021dd832a883e3
Oleh-Shynkarchuk <118341441+Oleh-Shynkarchuk@users.noreply.github.com>
2023-01-09 22:58:14 +0200

    feat(EEJT-0002) conflict resolved

--- REST API Basics/src/main/java/com/epam/esm/giftcertificates/repo/GiftCertificatesRepository.java
+++ REST API Basics/src/main/java/com/epam/esm/giftcertificates/repo/GiftCertificatesRepository.java
@@ -3,7 +3,6 @@ package com.epam.esm.giftcertificates.repo;
 import com.epam.esm.giftcertificates.entity.GiftCertificate;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public interface GiftCertificatesRepository {
